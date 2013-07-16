f2448378cfaf85ad8b5700a7bafabdbce1e229e9
Dennis Sell <sell.dennis@gmail.com>
2013-07-15 22:48:15 -0700

    Finished 41

--- Haskell/Euler41.hs
+++ Haskell/Euler41.hs
@@ -1,7 +1,27 @@
 import Data.Digits
+import Data.Maybe
 import Data.List
 
 permutation [] = [[]]
 permutation xs = [y| x <- xs, y <- map (x:) $ permutation $ delete x xs]
 
-all_pandigital_numbers = reverse . sort . map (unDigits 10) . permutation . digits 10 $ 123456789
+allPandigitalNumbers :: Integer -> [Integer]
+allPandigitalNumbers n = reverse . sort . map (unDigits 10) . permutation $ [1..n]
+
+isPrime :: Integer -> Bool
+isPrime n = loop n 2 n
+  where
+    loop n check limit  | check >= limit        =  True
+                        | mod n check == 0      =  False
+                        | otherwise             =  loop n (check + 1) limit
+
+largestPandigitalPrime :: Integer
+largestPandigitalPrime = loop 9
+  where
+    loop 1 = 1
+    loop n = case find isPrime (allPandigitalNumbers n) of
+                Just x -> x
+                Nothing -> loop (n - 1)
+ 
+
+
